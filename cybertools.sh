313530d0f07989dc65bf50f1f12062e5be47ca5f
wawanselow <58092268+wawanselow@users.noreply.github.com>
2019-12-07 17:55:33 +0700

    Add files via upload

---
+++ cybertools.sh
@@ -0,0 +1,449 @@
+clear
+bl='\033[34;1m' #blue
+gre='\033[32;1m' #green
+pur='\033[35;1m' #purles1
+cy='\033[36;1m' #cyan
+red='\033[31;1m' #red
+pe='\033[37;1m' #purple
+yw='\033[33;1m' #yellow
+
+sleep 1
+echo
+echo $bl"     ======"$bl"WELLCOME TO CYBER TOOLS"$bl"======"
+echo $bl"     =====##"$bl"INDONESIA CYBER TEAM"$bl"##====="
+
+sleep 3
+
+clear
+echo
+echo
+echo
+echo
+echo $red"           Sedang Melakukan Installasi Configurasi \033[00m1/5"
+echo $red"           Mohon tunggu !! . . ."   
+sleep 2
+echo '''\a
+\033[32;1m                              _
+\033[32;1m                             / |
+\033[32;1m                             | |
+\033[32;1m                             | |
+\033[32;1m                             |_| '...
+sleep 2
+clear
+echo
+echo
+echo
+echo
+echo $gre"           Sedang Melakukan Installasi Configurasi \033[00m2/5"
+echo $gre"           Mohon tunggu !! . . ."
+sleep 2
+echo '''\a
+\033[32;1m                             ____
+\033[32;1m                            |___ \
+\033[32;1m                             __) |
+\033[32;1m                            / __/
+\033[32;1m                           |_____| '...
+sleep 2
+clear
+echo
+echo
+echo
+echo
+echo $bl"           Sedang Melakukan Installasi Configurasi \033[00m3/5"
+echo $bl"           Mohon tunggu !! . . ."
+sleep 2
+echo '''\a
+\033[32;1m                            _____
+\033[32;1m                           |___ /
+\033[32;1m                             |_ \
+\033[32;1m                            ___) |
+\033[32;1m                           |____/ '...
+sleep 2
+clear
+echo
+echo
+echo
+echo
+echo $gre"            Sedang Melakukan Installasi Configurasi \033[00m4/5"
+echo $gre"            Mohon tunggu !! . . ."
+sleep 2
+echo '''\a
+\033[32;1m                            _  _
+\033[32;1m                           | || |
+\033[32;1m                           | || |_
+\033[32;1m                           |__   _|
+\033[32;1m                              |_| '...
+sleep 2
+clear
+echo
+echo
+echo
+echo
+echo $pur"           Sedang Melakukan Installasi Configurasi \033[00m5/5"
+echo $pur"           Mohon tunggu !! . . ."
+sleep 2
+echo '''\a
+\033[32;1m                             ____
+\033[32;1m                            | ___|
+\033[32;1m                            |___ \
+\033[32;1m                             ___) |
+\033[32;1m                            |____/ '...
+sleep 2
+echo $red"     INSTALLASI SELESAI!!"
+sleep 1
+clear
+echo
+echo "###############################################"
+figlet CyberTools | lolcat
+figlet Indonesia | lolcat
+echo
+echo "###############################################"
+echo "###############################################"
+echo $bl" ======================="$bl"======================="
+echo $bl" >"$red"  AUTHOR     :  CYBERTEAM"$red""
+echo $bl" >"$red"  Builder OS :  Linux (UNIX)"$red""
+echo $bl" >"$red"  Version    :  13.3.211"$red""
+echo $bl" >"$red"  Lang       :  Shell(.sh) | Python, Pythonpy, Python2, Python3(.py)"$red""
+echo $bl" >"$red"  Source     :  BY DEVILBLACK CYBERTEAM"$red""
+echo $bl" >"$red"  Country    :  INDONESIA (ID)"$red""
+echo $bl" ======================="$bl"======================="
+echo
+echo $bl" Input Choice:"
+echo "\033[00m Beberapa alat,tidak berfungsi.kami akan memperbaikinya ASAP."
+echo "\033[00m jika anda tidak mengetik input/input yang salah,maka akan keluar secara otomatis."
+sleep 1
+echo
+echo $yw" NB: (KETIK EXIT JIKA INGIN KELUAR)"
+echo $red" =================="$red"============================="
+echo $red" ["$bl"1"$red"]"$bl"  Report Facebook Account (With ID)"             ]
+echo $red" =================="$red"============================="
+echo $red" =================="$red"============================="
+echo $red" ["$bl"2"$red"]"$bl"  Brute Force Facebook"                                     ]
+echo $red" =================="$red"============================="
+echo $red" =================="$red"============================="
+echo $red" ["$bl"3"$red"]"$bl"  Brute Forcer for Facebook Account"             ]
+echo $red" =================="$red"============================="
+echo $red" =================="$red"============================="
+echo $red" ["$bl"4"$red"]"$bl"  Instainsane Brute Forcer"                                 ]
+echo $red" =================="$red"============================="
+echo $red" =================="$red"============================="
+echo $red" ["$bl"5"$red"]"$bl"  (Multi) Brute Forcer for Facebook Account"  ]
+echo $red" =================="$red"============================="
+echo $red" =================="$red"============================="
+echo $red" ["$bl"6"$red"]"$bl"  Cloning Yahoo (Login with Yahoo Account)"   ]
+echo $red" =================="$red"============================="
+echo $red" =================="$red"============================="
+echo $red" ["$bl"7"$red"]"$bl"  Open Source Information Facebook"             ]
+echo $red" =================="$red"============================="
+echo $red" =================="$red"============================="
+echo $red" ["$bl"8"$red"]"$bl"  Profil Guard (Shield) Facebook"                      ]
+echo $red" =================="$red"============================="
+echo $red" =================="$red"============================="
+echo $red" ["$bl"9"$red"]"$bl"  Bot React for Facebook"                                   ]
+echo $red" =================="$red"============================="
+echo $red" =================="$red"============================="
+echo $red" ["$bl"10"$red"]"$bl"  Spam Call"                                                        ]
+echo $red" =================="$red"============================="
+echo $red" =================="$red"============================="
+echo $red" ["$bl"11"$red"]"$bl"  Spam WhatsApp"                                            ]
+echo $red" =================="$red"============================="
+echo $red" =================="$red"============================="
+echo $red" ["$bl"12"$red"]"$bl"  Youtube DL"                                                      ]
+echo $red" =================="$red"============================="
+echo $red" =================="$red"============================="
+echo $red" ["$bl"13"$red"]"$bl"  Location"                                                          ]
+echo $red" =================="$red"============================="
+echo $red" =================="$red"============================="
+echo $red" ["$bl"14"$red"]"$bl"  Admin-finder"                                                   ]
+echo $red" =================="$red"============================="
+echo $red" =================="$red"============================="
+echo $red" ["$bl"15"$red"]"$bl"  Hack_Instagram"                                             ]
+echo $red" =================="$red"============================="
+echo $red" =================="$red"============================="
+echo $red" ["$bl"16"$red"]"$bl"  Install Metasploit"                                           ]
+echo $red" =================="$red"============================="
+echo $red" =================="$red"============================="
+echo $red" ["$bl"17"$red"]"$bl"  Wifi Hacker(root)"                                           ]
+echo $red" =================="$red"============================="
+echo $red" =================="$red"============================="
+echo $red" ["$bl"18"$red"]"$bl"  Install Nethunter"                                            ]
+echo $red" =================="$red"============================="
+echo $red" =================="$red"============================="
+echo $red" ["$bl"19"$red"]"$bl"  Termux_ubuntu"                                               ]
+echo $red" =================="$red"============================="
+echo $red" =================="$red"============================="
+echo $red" ["$bl"20"$red"]"$bl"  EXIT TOOLS INI"                                           ]
+echo $red" =================="$red"============================="
+echo $red" =================="$red"============================="
+
+echo "\033[00m Type \033[32;1msuper \033[00mto see the super tools."
+echo
+echo $gre" Hacked---> "$gre"Masukan Nomor \033[00m(1-20)"$yw":$gre"
+read -p " Hacked---> " pil
+
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "   1"|lolcat
+sleep 1
+git clone https://github.com/IlayTamvan/Report.git
+cd Report
+unzip Report.zip
+python2 Report.py
+fi
+
+if [ $pil = 2 ]
+then
+clear
+figlet -f slant "   2"|lolcat
+sleep 1
+git clone https://github.com/Senitopeng/fbbrute.git
+cd fbbrute
+python2 jomblo.py
+fi
+
+if [ $pil = 3 ]
+then
+clear
+figlet -f slant "   3"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/Fb-Cracker-v.3.git
+cd Fb-Cracker-v.3
+python2 crack.py
+fi
+
+if [ $pil = 4 ]
+then
+clear
+figlet -f slant "   4"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/instainsane.git
+sleep 1
+clear
+echo
+echo '\033[32;1m Entering Directory...'
+sleep 2
+cd instainsane
+clear
+echo
+echo '\033[32;1m chmod-ing shell...'
+sleep 2
+chmod +x instainsane.sh && chmod +x install.sh
+clear
+echo
+echo '\033[32;1m Installing...'
+sleep 5
+clear
+echo
+echo '\033[32;1m Everything is installed, Time to execute the command...'
+sleep 10
+clear
+echo
+echo '\033[32;1m Type "./install.sh && ./instainsane.sh" in ur terminal to run this program, or copying that commands.'
+sleep 3
+sudo su
+sleep 1
+exit
+fi
+
+if [ $pil = 5 ]
+then
+clear
+figlet -f slant "   5"|lolcat
+sleep 1
+git clone https://github.com/tikuskecil/multi-bruteforce-facebook.git
+cd multi-bruteforce-facebook
+python2 MBF.py
+fi
+
+if [ $pil = 6 ]
+then
+clear
+figlet -f slant "   6"|lolcat
+sleep 1
+git clone https://github.com/wahyuandhika/YahooCloning.git
+cd YahooCloning
+pip2 install requests mechanize
+pip2 install requirements
+python2 cloning.py
+fi
+
+if [ $pil = 7 ]
+then
+clear
+figlet -f slant "   7"|lolcat
+sleep 1
+git clone https://github.com/CiKu370/OSIF.git
+cd OSIF
+pip2 install -r requirements.txt
+python2 osif.py
+fi
+
+if [ $pil = 8 ]
+then
+clear
+figlet -f slant "   8"|lolcat
+sleep 1
+git clone https://github.com/jaxBCD/FBshield.git
+cd FBshield
+python2 guard.py
+fi
+
+if [ $pil = 9 ]
+then
+clear
+figlet -f slant "   9"|lolcat
+sleep 1
+git clone https://github.com/edi-nasa12/ROBOT.git
+cd ROBOT
+python2 blackbot.py
+fi
+
+if [ $pil = 10 ]
+then
+clear
+figlet -f slant "   1 0"|lolcat
+sleep 1
+git clone https://github.com/joss24242/prank
+cd prank
+php call.php
+fi
+
+if [ $pil = 11 ]
+then
+clear
+figlet -f slant "   1 1"|lolcat
+sleep 1
+git clone https://github.com/4L13199/LITESPAM.git
+cd LITESPAM
+sh LITESPAM.sh
+fi
+
+if [ $pil = 12 ]
+then
+clear
+figlet -f slant "   1 2"|lolcat
+sleep 1
+git clone https://github.com/rahmadxyz/sub-bot.git
+cd sub-bot
+php sub-bot.php
+fi
+
+if [ $pil = 13 ]
+then
+clear
+figlet -f slant "   1 3"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/locator.git
+cd locator
+bash locator.sh
+fi
+
+if [ $pil = 14 ]
+then
+clear
+figlet -f slant "   1 4"|lolcat
+sleep 1
+git clone  https://github.com/the-c0d3r/admin-finder.git
+cd admin-finder
+python2 admin-finder.py
+fi
+
+if [ $pil = 15 ]
+then
+clear
+figlet -f slant "   1 5"|lolcat
+sleep 1
+git clone https://github.com/rrudy0966/instatools
+cd instatools
+unzip node_modules.zip
+ls
+fi
+
+if [ $pil = 16 ]
+then
+clear
+figlet -f slant "   1 6"|lolcat
+sleep 1
+git clone https://github.com/Hax4us/Metasploit_termux
+cd Metasploit_termux
+chmod +x metasploit.sh
+sh metasploit.sh
+fi
+
+if [ $pil = 17 ]
+then
+clear
+figlet -f slant "   1 7"|lolcat
+sleep 1
+git clone https://github.com/esc0rtd3w/wifi-hacker
+cd wifi-hacker
+chmod +x wifi-hacker.sh
+fi
+
+if [ $pil = 18 ]
+then
+clear
+figlet -f slant "   1 8"|lolcat
+sleep 1
+git clone https://github.com/Hax4us/Nethunter-In-Termux.git
+cd Nethunter
+chmod +x kalinethunter
+./kalinethunter
+./startkali.sh
+fi
+
+if [ $pil = 19 ]
+then
+clear
+figlet -f slant "   1 9"|lolcat
+sleep 1
+git clone https://github.com/Neo-Oli/termux-ubuntu
+cd termux-ubuntu
+chmod 777 ubuntu.sh
+sh ubuntu.sh
+fi
+
+if [ $pil = 20 ]
+then
+clear
+figlet -f slant "   EXIT"|lolcat
+sleep 3
+clear
+echo
+echo
+echo $gre" Terimakasih telah menggunakan script saya!"
+echo
+sleep 2
+echo $gre" Script Shell Ini Dibuat Pada : December 01, 2019"
+echo $gre" @ 03.21 AM | INDONESIA (ID)"
+echo
+sleep 2
+echo $gre" Contact Me!"
+echo
+echo
+sleep 2
+echo $red" ======================="$red"======================"
+echo $red" XXX"$bl"  AUTHOR    :  CYBERTEAM"$bl"                                   XXX"
+echo $red" XXX"$bl"  WhatsApp  :  0823-9207-3951"$bl"                           XXX"
+echo $red" XXX"$bl"  GMAIL     :  Wawanseloww0491@gmail.com"$bl"  XXX"
+echo $red" XXX"$bl"  Facebook  :  Rahasia"$bl"                                            XXX"
+echo $red" XXX"$bl"  Name :  Mr.WawaNseloww"$bl"                                 XXX"
+echo $red" ======================="$red"======================="
+
+figlet Salam | lolcat
+figlet Cyber | lolcat
+figlet Indonesia | lolcat
+echo
+echo
+sleep 2
+echo $gre" Sampai Jumpa! Dan Terimakasih"
+echo
+sleep 1
+echo $gre" Keluar Tools . . ."
+echo
+echo
+fi
+
+#execute
+exit
